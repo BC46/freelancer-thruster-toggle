717ea369215c2c9b1032707d09389ecc0668e622
C.S. Melis <cees3331x00@gmail.com>
2023-04-14 00:04:06 +0200

    Add thruster_toggle files

---
+++ thruster_toggle.cpp
@@ -0,0 +1,16 @@
+#include <windows.h>
+#include <stdio.h>
+
+void Start() {
+    printf("Test");
+}
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved) {
+    UNREFERENCED_PARAMETER(hinstDLL);
+    UNREFERENCED_PARAMETER(lpReserved);
+
+    if (fdwReason == DLL_PROCESS_ATTACH)
+        Start();
+
+    return TRUE;
+}
