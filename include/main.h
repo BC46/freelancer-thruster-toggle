d6e658819fe7febcb34e7a5c669d4949a04c1eab
C.S. Melis <cees3331x00@gmail.com>
2023-04-18 12:25:53 +0200

    Add documentation for main functions

--- include/main.h
+++ include/main.h
@@ -1,5 +1,10 @@
+#pragma once
+
 #include <windows.h>
 
+/// @brief Gathers all the configs and hooks in order for the Thruster Toggle functionality to start working.
+/// @return BOOL: Whether or not the functionality has been able to start.
 BOOL Start();
 
+/// @brief DLL main entry point.
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved);
